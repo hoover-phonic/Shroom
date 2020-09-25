45a7c31cd3c390bcae02412b6c4d42ec8974a70a
Ipshita Diwan <24778311+hoover-phonic@users.noreply.github.com>
2020-09-25 23:50:24 +0530

    Added Expense List  and its individual components

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
@@ -10,7 +11,7 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill' }));
+store.dispatch(addExpense({ description: 'Water Bill', amount: 683}));
 store.dispatch(addExpense({ description: 'Gas Bill' }));
 store.dispatch(setTextFilter('water'));
 
@@ -18,5 +19,11 @@ const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
-ReactDOM.render(<AppRouter />, document.getElementById('app')); 
+const jsx = (
+  <Provider store = {store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app')); 
 
